5d4a5626d1c7106c5cee34c5ad2cf914bc4b4081
Ananthamoorthy <ananth290903@gmail.com>
2025-06-27 21:40:37 +0530

    Jun 27 21:40PM

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -5,3 +5,21 @@ from .models import Book
 def index(request):
     books=Book.objects.all()
     return render(request,"book_outlet/index.html",{"books":books})
+
+def book_detail(request,id):
+    book=Book.objects.get(pk=id)
+    return render(request,"book_outlet/book_detail.html",{"title":book.title,"author":book.author,"rating":book.rating,"is_bestselling":book.is_bestselling})
+
+
+
+
+'''
+def book_detail(request,id):
+    book=Book.objects.get(pk=id)
+    return render(request,"book_outlet/book_detail.html",
+    {"title":book.title ,
+     "author":book.author,
+     "is_bestseller":book.is_bestseller,
+     "rating":book.rating})
+
+'''
